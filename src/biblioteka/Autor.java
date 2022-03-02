134e9a46b6e973aa9b8bedefc69345201d5c48b6
was2309 <vasilijem92@gmail.com>
2022-03-02 10:26:55 +0100

    Refaktorisana klasa Knijga, uvedena klasa Autor

---
+++ src/biblioteka/Autor.java
@@ -0,0 +1,24 @@
+package biblioteka;
+
+public class Autor {
+	
+	private String ime;
+	private String prezime;
+	
+	public String getIme() {
+		return ime;
+	}
+	
+	public String getPrezime() {
+		return prezime;
+	}
+	
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	
+}
--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,10 +1,12 @@
 package biblioteka;
 
+import java.util.List;
+
 public class Knjiga {
 	
 	private String naslov;
 	private long isbn;
-	private String autori;
+	private List<Autor> autori;
 	private String izdavac;
 	private int izdanje;
 	
@@ -26,11 +28,11 @@ public class Knjiga {
 		this.isbn = isbn;
 	}
 	
-	public String getAutori() {
+	public List<Autor> getAutori() {
 		return autori;
 	}
 	
-	public void setAutori(String autori) {
+	public void setAutori(List<Autor> autori) {
 		this.autori = autori;
 	}
 	
